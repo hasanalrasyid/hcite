b17193c232e5e8888ee069016ebc699ed0eb942b
aku <aku@ASUS>
2020-02-25 10:49:28 +0900

    initialization of hcite v0.2.0.0

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ reflex-platform ? import ./reflex-platform {
+  config.android_sdk.accept_license = true;
+  } }:
+
+reflex-platform.project ({ pkgs, ... }: {
+  packages = {
+    common = ./common;
+    backend = ./backend;
+    frontend = ./frontend;
+  };
+
+  android.frontend = {
+    executableName = "frontend";
+    applicationId = "org.example.frontend";
+    displayName = "Example Android App";
+  };
+
+  ios.frontend = {
+    executableName = "frontend";
+    bundleIdentifier = "org.example.frontend";
+    bundleName = "Example iOS App";
+  };
+
+  shells = {
+    ghc = ["common" "backend" "frontend"];
+    ghcjs = ["common" "frontend"];
+  };
+})
