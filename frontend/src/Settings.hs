43ec7633d35acf753b352fddf6a641dc87f4d402
aku <aku@ASUS>
2020-04-10 19:04:13 +0900

    prototyping

--- frontend/src/Settings.hs
+++ frontend/src/Settings.hs
@@ -11,6 +11,6 @@ import qualified Data.Text as T
 
 
 serverBackend :: T.Text
---serverBackend = "http://127.0.0.1:3000/"
-serverBackend = "http://192.168.43.175:3000/"
+serverBackend = "http://127.0.0.1:3000/"
+--serverBackend = "http://192.168.43.175:3000/"
 
